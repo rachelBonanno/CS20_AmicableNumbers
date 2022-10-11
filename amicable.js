883c3ce67c5d86e16fbb6c3aaa6087c3dc36dfc3
rachelBonanno <rachel.bonanno@tufts.edu>
2022-10-11 15:53:40 -0400

    math

--- amicable.js
+++ amicable.js
@@ -1,2 +1,26 @@
-var num1 = prompt("First number:");
-var num2 = prompt("Second number:");
+function inputnum(form) {
+    var TestVar = form.inputbox.value;
+    alert ("You typed: " + TestVar);
+}
+
+function amicable(x) {
+    let result = 0;
+    for (let i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i == 0) {
+            if (i == (x / i)) {
+                result += i;
+            }
+        } else {
+            result += (i + x / i);
+        }
+    }
+    return (result + 1);
+}
+
+function factors(x) {
+    for(let i = 1; i <= x; i++) {
+        if(num % i == 0) {
+            console.log(i);
+        }
+    }
+}
