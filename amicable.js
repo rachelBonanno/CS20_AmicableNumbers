ef99b1cd8baf5df9067b0af768dc966e8d5b8901
rachelBonanno <rachel.bonanno@tufts.edu>
2022-10-11 22:57:44 -0400

    math

--- amicable.js
+++ amicable.js
@@ -1,15 +1,22 @@
-function inputnum(form) {
-    const form = document.getElementById('submit');
-    const numone = form.elements['numone'];
-    const numtwo = form.elements['numtwo'];
-    let numberone = numone.value;
-    let numbertwo = numtwo.value;
-    form.addEventListener('submit', (event) => {
-        // handle the form data
-    });
+function inputnum() {
+    var numberone = document.getElementById.elements("numone").value;
+    var numbertwo = document.getElementById.elements("numtwo").value;
+
+    var sumone = 0;
+    var sumtwo = 0;
+
+    var arraynumone = [];
+    var arraynumtwo = [];
+
+    factorsSum(arraynumone, numone);
+    factorsSum(arraynumtwo, numtwo);
+    document.getElementById("result1").innerHTML = "The numbers:  " + numberone + " and " + numbertwo  + " are amicable";
+    document.getElementById("result2").innerHTML = "The numbers:  " + numberone + " and " + numbertwo + " are amicable";
+
+
 }
 
-function amicable(x) {
+function amicable (x) {
     let result = 0;
     for (let i = 2; i <= Math.sqrt(x); i++) {
         if (x % i == 0) {
@@ -23,14 +30,16 @@ function amicable(x) {
     return (result + 1);
 }
 
-function factors(x) {
-    const fact = new Array();
-    const fact = [];
-    let w = 0;
-    for (let i = 1; i <= x; i++) {
-        if (num % i == 0) {
-            fact[w] = i;
-            w++;
-        }
+function factors (array, fact, num) {
+    if (num % fact == 0) {
+            array.push(fact);
+            return fact;
+    }
+    return 0;
+}
+
+function sum (array, sum, num) {
+    for (let i = 1; i <= num; i++) {
+
     }
 }
